dd1b7a0e41a3e7f6c90a28ed9f38f40b275e2246
Yoram <yoramjanssenn@gmail.com>
2024-08-22 05:28:59 +0200

    feat(utility)!: add `exchange` function, add `call_result_t` type alias, add `scope_guard` type and add `get_value_with_in`
    Signed-off-by: Yoram <yoramjanssenn@gmail.com>

--- include/multicore/utility.hpp
+++ include/multicore/utility.hpp
@@ -29,7 +29,20 @@
 #ifndef MTC_UTILITY_HPP
 #define MTC_UTILITY_HPP
 
+#include "concepts.hpp"
+
+#include <type_traits>
+
+#define MTC_FWD(...) static_cast<decltype(__VA_ARGS__) &&>(__VA_ARGS__)
+#define MTC_FWD_T(t, ...) static_cast<t &&>(__VA_ARGS__)
+#define MTC_MOVE(...) static_cast<typename mtc::detail::remove_reference<decltype(__VA_ARGS__)>::type &&>(__VA_ARGS__)
+
 namespace mtc {
+
+  /**
+   * \defgroup utility utility
+   * \brief The `utility` module provides utilities used throughout the library.
+   */
 
   namespace detail {
     template <class T>
@@ -47,22 +60,68 @@ namespace mtc {
       using type = T;
     };
 
-    template<class>
+    template <class>
     inline constexpr auto always_false = false;
   }  // namespace detail
+
+  template <template <class> class>
+  struct template_alias {};
 
   template <class T>
   auto as_lvalue(T &&) -> T &;
 
+  /// \ingroup utility
+  /// \brief The `mtc::declval` function converts any type `T` to an rvalue reference of type `T`, making it possible to use
+  /// member functions in the operand of the `decltype` specifier without the need to construct an instance of `T`. Can
+  /// only be used in unevaluated contexts.
+  /// \tparam T The type to convert to an rvalue reference.
+  /// \return Cannot be called and thus never returns a value. The return type is `T &&`.
   template <class T>
-  auto declval() noexcept -> T && {
+  constexpr auto declval() noexcept -> T && {
     static_assert(detail::always_false<T>, "Calling declval is ill-formed");
   }
 
-}  // namespace mtc
+  /// \ingroup utility
+  /// \brief The `mtc::exchange` function replaces the value of `obj` with `new_value` and returns the old value of `obj`.
+  /// \tparam T The type of the object to replace.
+  /// \tparam U The type of the new value.
+  /// \param obj A reference to the object whose value to replace.
+  /// \param new_value The new value to assign to `obj`.
+  /// \return The old value of `obj`.
+  template <class T, class U = T>
+    requires std::is_move_constructible_v<T> && std::is_assignable_v<T &, U>
+  constexpr auto exchange(T &obj, U &&new_value) noexcept(std::is_nothrow_move_constructible_v<T> && std::is_nothrow_assignable_v<T &, U>) -> T {
+    T old_value = MTC_MOVE(obj);
+    obj = MTC_FWD(new_value);
+    return old_value;
+  }
 
-#define MTC_FWD(...) static_cast<decltype(__VA_ARGS__) &&>(__VA_ARGS__)
-#define MTC_FWD_T(t, ...) static_cast<t &&>(__VA_ARGS__)
-#define MTC_MOVE(...) static_cast<typename mtc::detail::remove_reference<decltype(__VA_ARGS__)>::type &&>(__VA_ARGS__)
+  template <class Fn, class... Args>
+  using call_result_t = decltype(declval<Fn>()(declval<Args>()...));
+
+  template <template <class...> class Tag>
+  static constexpr auto get_value_with_in() noexcept -> void {}
+
+  template <template <class...> class Tag, class Arg, class... Args>
+  [[nodiscard]] static constexpr auto get_value_with_in(Arg &&arg, Args &&...args) noexcept -> decltype(auto) {
+    if constexpr (instance_of<std::decay_t<Arg>, Tag>) {
+      return MTC_FWD(arg);
+    } else {
+      return get_value_with_in<Tag>(MTC_FWD(args)...);
+    }
+  }
+
+  template <class Fn>
+  struct scope_guard : Fn {
+    constexpr explicit scope_guard(Fn &&fn) noexcept : Fn(MTC_MOVE(fn)) {}
+    constexpr scope_guard(const scope_guard &) = delete;
+    constexpr scope_guard(scope_guard &&) = delete;
+    constexpr ~scope_guard() { this->operator()(); }
+
+    constexpr auto operator=(const scope_guard &) -> scope_guard & = delete;
+    constexpr auto operator=(scope_guard &&) -> scope_guard & = delete;
+  };
+
+}  // namespace mtc
 
 #endif  // MTC_UTILITY_HPP
